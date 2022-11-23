30ba45a56d96668feb71ac5c882f9beb1925a24b
Demian Vega <506013@SRYAS.CDMX>
2022-11-23 13:53:31 -0600

    3rd

--- src/components/Core.vue
+++ src/components/Core.vue
@@ -1,21 +1,25 @@
 <template>
 <div class="Core">
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-
+    <div v-for="image in images" class="Post ">
+        <Namep class="Name"/>
+        <!-- <div class="Name">Name</div> -->
+        <img :src=image.url :key="url" class="Image"/>
+        <p class="descrp">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Adipisci et architecto sequi nisi, fugiat 
+            distinctio illo temporibus rerum officiis, excepturi 
+            facere corporis est eum molestiae dolorem laudantium 
+            mollitia ullam maiores.</p>
+        <div class="Likes">Likes</div>
+    </div>
+    
 </div>
 </template>
 <script>
-import Post from '@/components/Post.vue'
+import Namep from '@/components/Namep.vue'
 export default {
     name: 'Core',
     components:{
-        Post,
+        Namep,
     },data(){
         return{
             images:[
@@ -41,5 +45,35 @@ export default {
 }
 ::-webkit-scrollbar {
     display: none;
+}
+.Image{
+    /* background-image: url('https://data.whicdn.com/images/357279301/original.jpg'); */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0.5em 0;
+}
+.Post{
+    height: auto;
+    background-color: rgba(103, 103, 103, 0.466);
+    margin: 1em;
+    display: grid;
+    grid-template-rows: 50px auto auto 50px;
+    border-radius: 10px;
+    padding-top: 1em;
+    /* overflow-y: scroll; */
+}
+.Likes {
+    margin: 0;
+}
+p{
+    margin: 1em;
+}
+.descrp{
+    text-align: justify;
+    font-style: oblique;
 }
 </style>
---
+++ src/components/Namep.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="wrapp">
+        <div class="wrappleft">
+
+            <div class="MiniImg" />
+            <div class="wrapdate">
+                <h3>Name</h3>
+                <div class="date">
+                    <p>{{currentDate()}}</p>
+                    
+                </div>
+            </div>
+
+        </div>
+
+        <p class="dots">⋮</p>
+    </div>
+</template>
+<script>
+export default{
+    name: 'Namep',
+    methods: {
+        currentDate() { 
+            // var time = setTimeout(()=>{ 
+            //     new Date().toLocaleTimeString()
+            // }, 500)
+
+            return (
+                (new Date().toLocaleDateString())
+            + 
+            ' || ' 
+            + 
+            // time
+            (new Date().toLocaleTimeString())   
+            // setTimeout(()=>{new Date().toLocaleTimeString()}, 1000)
+            ) 
+        }
+        }   
+}
+</script>
+<style scooped>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+h3{
+    font-size: 15px;
+    
+}
+.wrapp{
+    display: grid;
+    grid-template-columns: 70% 30%;
+}
+.wrappleft{
+    display: grid;
+    grid-template-columns: 70px auto ;
+    text-align: start;
+}
+.wrappleft p{
+    font-size: 12px;
+    margin: 3px 0;
+}
+.MiniImg{
+    background-image: url('https://ae01.alicdn.com/kf/Hd5d842e9125b492c91b3170e9f9c5e0bA/MPK-Store-Cat-Toy-Cat-Costume-Clip-Tie-for-Cats-and-Dogs-Cat-Collar.jpg_Q90.jpg_.webp');
+    background-color: aquamarine;
+    background-size: cover;
+    background-position: center;
+    border-radius: 5px;
+    height: 50px;
+    width: 50px;
+    margin: 0  0.5em ;
+}
+.dots{
+    font-weight: bold;
+    font-size: 2.5em;
+    margin: 0;
+    padding-right: 0.5em;
+    text-align: end;
+    cursor: pointer;
+    color: rgb(97, 103, 103);
+}
+.date p{
+    font-style: oblique;
+    font-size: 10px;
+}
+
+</style>
